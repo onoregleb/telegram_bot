bf4b2656c2eac324729ab65e29ca543a4dceb759
onore_gleb <onoreg@mail.ru>
2024-09-08 12:41:52 +0300

    Initial commit

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import nest_asyncio
+
 nest_asyncio.apply()
 
 from telegram import Update
@@ -14,7 +15,6 @@ from oauth2client.service_account import ServiceAccountCredentials
 from datetime import datetime
 import asyncio
 
-
 scope = [
     "https://spreadsheets.google.com/feeds",
     "https://www.googleapis.com/auth/spreadsheets",
@@ -29,11 +29,11 @@ client = gspread.authorize(creds)
 spreadsheet = client.open("test")  # Замените на имя вашей таблицы
 sheet = spreadsheet.sheet1
 
-# Функция, которая срабатывает при нажатии на /start
+
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     await update.message.reply_text("Добрый день, вы можете оставить обратную связь")
 
-# Функция, которая срабатывает при получении сообщения
+
 async def handle_feedback(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     user = update.message.from_user
     feedback = update.message.text
@@ -45,20 +45,18 @@ async def handle_feedback(update: Update, context: ContextTypes.DEFAULT_TYPE) ->
 
     await update.message.reply_text("Спасибо за обратную связь")
 
+
 async def main() -> None:
     # Замените 'YOUR_TOKEN' на токен вашего бота
     app = ApplicationBuilder().token("YOUR_TOKEN").build()
 
-    # Обработчик команды /start
     app.add_handler(CommandHandler("start", start))
 
-    # Обработчик текстовых сообщений
     app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_feedback))
 
-    # Запуск бота
     await app.run_polling()
 
-# Запуск основного цикла
+
 if __name__ == "__main__":
     try:
         asyncio.run(main())
